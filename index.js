81b8ffdc45f4d9fd970359c842283c0bb51a2f22
Najib <44539538+N4J13@users.noreply.github.com>
2024-03-29 00:29:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ require("dotenv").config();
 const { onStart, onCallbackQuery } = require("./handlers/onboardHandler.js");
 const ffmpeg = require("fluent-ffmpeg");
 const bot = require("./config/bot.js");
-const { trimVideo } = require("./utils/trimVideo.js");
-const { getTotalSeconds } = require("./utils/durationUtils.js");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-ffmpeg.setFfmpegPath( process.env.FFMPEG_PATH || "C:/ffmpeg/bin/ffmpeg.exe");
+ffmpeg.setFfmpegPath(process.env.FFMPEG_PATH || "C:/ffmpeg/bin/ffmpeg.exe");
 ffmpeg.setFfprobePath(process.env.FFMPEG_PROBE_PATH || "C:/ffmpeg/bin");
 ffmpeg.setFlvtoolPath(process.env.FFMPEG_FLV_TOOL_PATH || "C:/flvtool");
 
@@ -18,12 +16,9 @@ app.get("/", async (req, res) => {
   res.send("Hello World!");
 });
 
-
-
 bot.onText(/\/start/, (msg) => onStart(msg));
 
 bot.on("callback_query", (callbackQuery) => onCallbackQuery(callbackQuery));
-
 
 app.listen(PORT, () => {
   console.log("Server is running on port" + PORT);
